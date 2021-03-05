548280750f4332092717c6a4005b2dbd97ca5352
tsubasa.katayama <tsubanetoon@yahoo.co.jp>
2021-03-05 06:32:55 +0000

    syuusei

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
 
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
   
-  has_many :relationships
+  has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
-  has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :followers, through: :reverses_of_relationship, source: :user, dependent: :destroy
 
   def follow(other_user)
     unless self == other_user
@@ -34,7 +34,7 @@ class User < ApplicationRecord
   
   
   
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :likes, through: :favorites, source: :micropost
   
   def favorite(other_micropost)
